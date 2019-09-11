2c86a877ec0c3ff6cf175976d45d9dd6cfddb118
weipengda <weipengda@xueleyun.com>
2019-09-11 15:53:11 +0800

    add

--- js/canvas.js
+++ js/canvas.js
@@ -1,1 +1,31 @@
-const canvasDrawing = document.querySelector("#Drawing");
+// const canvasDrawing = document.querySelector("#Drawing");
+
+
+if (!Function.prototype.softBind) {
+    Function.prototype.softBind = function (obj) {
+        var fn = this;
+        // 捕获所有 curried 参数
+        var curried = [].slice.call(arguments, 1);
+        var bound = function () {
+            return fn.apply(
+                (!this || this === (window || global)) ?
+                    obj : this, curried.concat.apply(curried, arguments)
+            );
+        };
+        bound.prototype = Object.create(fn.prototype);
+        return bound;
+    };
+}
+function foo() {
+    console.log("name: " + this.name);
+}
+var obj = { name: "obj" },
+    obj2 = { name: "obj2" },
+    obj3 = { name: "obj3" };
+// var fooOBJ = foo.softBind(obj);
+// fooOBJ(); // name: obj
+obj2.foo = foo.softBind(obj);
+obj2.foo(); // name: obj2 <---- 看！！！
+// fooOBJ.call(obj3); // name: obj3 <---- 看！
+// setTimeout(obj2.foo, 10);
+    // name: obj <---- 应用了软绑定
