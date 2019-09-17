488be1fbf47a3fa75db9e29aacfb3db1f654344b
weipengda <weipengda@xueleyun.com>
2019-09-17 09:27:23 +0800

    add

--- js/canvas.js
+++ js/canvas.js
@@ -1,31 +1,30 @@
-// const canvasDrawing = document.querySelector("#Drawing");
+const canvasDrawing = document.querySelector("#Drawing");
+const context = canvasDrawing.getContext("2d");
+// context.strokeStyle = "#00f";
+// context.rect(0,0,50,50);  
+// context.stroke();
+
+// context.font = '24px STHeiTi, SimHei';
+// // 文本信息对象就有了
+// var textZh = context.measureText('帅');
+// var textEn = context.measureText('handsome');
+// // 文字绘制
+// context.fillText('帅', 60, 50);
+// context.fillText('handsome', 60, 90);
+// // 显示宽度
+// context.font = '12px Arial';
+// context.fillStyle = 'red';
+// context.fillText('宽' + textZh.width, 62 + textZh.width, 40);
+// context.fillText('宽' + textEn.width, 62 + textEn.width, 80);
 
 
-if (!Function.prototype.softBind) {
-    Function.prototype.softBind = function (obj) {
-        var fn = this;
-        // 捕获所有 curried 参数
-        var curried = [].slice.call(arguments, 1);
-        var bound = function () {
-            return fn.apply(
-                (!this || this === (window || global)) ?
-                    obj : this, curried.concat.apply(curried, arguments)
-            );
-        };
-        bound.prototype = Object.create(fn.prototype);
-        return bound;
-    };
-}
-function foo() {
-    console.log("name: " + this.name);
-}
-var obj = { name: "obj" },
-    obj2 = { name: "obj2" },
-    obj3 = { name: "obj3" };
-// var fooOBJ = foo.softBind(obj);
-// fooOBJ(); // name: obj
-obj2.foo = foo.softBind(obj);
-obj2.foo(); // name: obj2 <---- 看！！！
-// fooOBJ.call(obj3); // name: obj3 <---- 看！
-// setTimeout(obj2.foo, 10);
-    // name: obj <---- 应用了软绑定
+// 文字样式
+context.font = '50px STHeiti, SimHei';
+// 文字先描边
+context.lineWidth = 3;
+context.strokeStyle = 'red';
+context.strokeText('文字描边', 50, 90);
+// 再填充
+// context.fillText('文字描边', 50, 90);
+// 登录状态下不会出现这行文字，点击页面右上角一键登录
+
