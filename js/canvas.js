b77c3ac9751bc24bd3825f4df38f5f5a5565c216
weipengda <weipengda@xueleyun.com>
2019-09-24 09:01:29 +0800

    add

--- js/canvas.js
+++ js/canvas.js
@@ -19,12 +19,29 @@ const context = canvasDrawing.getContext("2d");
 
 
 // 文字样式
-context.font = '50px STHeiti, SimHei';
-// 文字先描边
-context.lineWidth = 3;
-context.strokeStyle = 'red';
-context.strokeText('文字描边', 50, 90);
+// context.font = '50px STHeiti, SimHei';
+// // 文字先描边
+// context.lineWidth = 3;
+// context.strokeStyle = 'red';
+// context.strokeText('文字描边', 50, 90);
 // 再填充
 // context.fillText('文字描边', 50, 90);
 // 登录状态下不会出现这行文字，点击页面右上角一键登录
+
+
+
+// 文字背景为图片
+var img = new Image;
+img.onload = function(){
+    draw();
+};
+img.src = "../images/test.jpg";
+function draw(){
+    var pattern = context.createPattern(img,"no-repeat");
+    context.fillStyle = pattern;
+// 登录状态下不会出现这行文字，点击页面右上角一键登录
+    context.textBaseline = "top";
+    context.font = '200px STHeiti, SimHei';
+    context.fillText("你好啊哈哈",0,0);
+}
 
