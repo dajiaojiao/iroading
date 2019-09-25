75ed8e227cac374657e159b3af15f267a0f437b6
weipengda <weipengda@xueleyun.com>
2019-09-25 09:10:05 +0800

    add

--- installServiceWorker.js
+++ installServiceWorker.js
@@ -1,24 +1,24 @@
-// if('serviceWorker' in navigator){
-//     navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
-//         console.log('ServiceWorker registration successful with scope:',registration)
-//     })
-//     .catch (error => {
-//         console.log(error);
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
+        console.log('ServiceWorker registration successful with scope:',registration)
+    })
+    .catch (error => {
+        console.log(error);
         
-//     })
-// }
-// const img = document.querySelector("#img1");
-// setTimeout(function(){
-//     img.src = "./images/test22.jpg"
-// },1000)
-unInstall()
-function unInstall() {
-    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-        for (let registration of registrations) {
-            console.log(registration)
-            registration.unregister();
-        }
-    });
+    })
 }
+const img = document.querySelector("#img1");
+setTimeout(function(){
+    img.src = "./images/test.jpg"
+},1000)
+// unInstall()
+// function unInstall() {
+//     navigator.serviceWorker.getRegistrations().then(function (registrations) {
+//         for (let registration of registrations) {
+//             console.log(registration)
+//             registration.unregister();
+//         }
+//     });
+// }
 
  
--- serviceWorker.js
+++ serviceWorker.js
@@ -2,9 +2,9 @@ const cacheName = "testCache5"
 self.addEventListener("install",event => {
     event.waitUntil(Promise.all([caches.open(cacheName)
         .then(cache => {
-            cache.addAll([
-                './images/test.jpg'
-            ]);
+            // cache.addAll([
+            //     './images/test.jpg'
+            // ]);
         }),
         self.skipWaiting()])
         
@@ -19,33 +19,33 @@ self.addEventListener("activate",(event) => {
 self.addEventListener("fetch",(event) => {
     console.log(event)
     if(/\.jpg$/.test(event.request.url)){
-        event.respondWith(fetch('/images/test.jpg'));
-        return;
+        // event.respondWith(fetch('/images/test.jpg'));
+        // return;
     }
 
-    event.respondWith(
-        caches.match(event.request)
-        .then((response) => {
-            if(response){
-                return response
-            }
-            var requestToCache = event.request.clone();
-            return fetch(requestToCache).then((res) => {
-                if(!res || res.status !== 200){
-                    return res;
-                }
+    // event.respondWith(
+    //     caches.match(event.request)
+    //     .then((response) => {
+    //         if(response){
+    //             return response
+    //         }
+    //         var requestToCache = event.request.clone();
+    //         return fetch(requestToCache).then((res) => {
+    //             if(!res || res.status !== 200){
+    //                 return res;
+    //             }
 
-                if(!event.request.clone().url.includes(".html")){
-                    var res1 = res.clone();
-                    caches.open(cacheName)
-                    .then((cache) => {
-                        cache.put(requestToCache.url,res1)
-                    })
-                }
+    //             if(!event.request.clone().url.includes(".html")){
+    //                 var res1 = res.clone();
+    //                 caches.open(cacheName)
+    //                 .then((cache) => {
+    //                     cache.put(requestToCache.url,res1)
+    //                 })
+    //             }
                 
-                return res
-            })
-        })
+    //             return res
+    //         })
+    //     })
 
-    )
+    // )
 })
