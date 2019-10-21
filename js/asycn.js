0393373679a7f64ff6f22377c0d2e229afb61580
weipengda <weipengda@xueleyun.com>
2019-10-21 09:48:01 +0800

    add

--- js/asycn.js
+++ js/asycn.js
@@ -51,3 +51,73 @@
 // console.log(f.next());
 // a = 2;
 // f.next(1);
+
+
+//语法糖
+// let test = function () {
+//     // ret 为一个Promise对象，因为ES6语法规定 async 函数的返回值必须是一个 promise 对象
+//     let ret = _asyncToGenerator(function* () {
+//         for (let i = 0; i < 10; i++) {
+//             let result = yield sleep(1000);
+//             console.log(result);
+//         }
+//     });
+//     return ret;
+// }();
+
+// // generator 自执行器
+// function _asyncToGenerator(genFn) {
+//     return new Promise((resolve, reject) => {
+//         let gen = genFn();
+//         function step(key, arg) {
+//             let info = {};
+//             try {
+//                 info = gen[key](arg);
+//             } catch (error) {
+//                 reject(error);
+//                 return;
+//             }
+//             if (info.done) {
+//                 resolve(info.value);
+//             } else {
+//                 return Promise.resolve(info.value).then((v) => {
+//                     return step('next', v);
+//                 }, (error) => {
+//                     return step('throw', error);
+//                 });
+//             }
+//         }
+//         step('next');
+//     });
+// }
+
+function wait() {
+    return new Promise(resolve =>
+        setTimeout(resolve, 10 * 1000)
+    )
+}
+
+async function main() {
+    console.time();
+    const x = await wait(); // 每个都是都执行完才结,包括setTimeout（10*1000）的执行时间
+    const y = await wait(); // 执行顺序 x->y->z 同步执行，x 与 setTimeout 属于同步执行
+    const z = await wait();
+    console.timeEnd(); // default: 30099.47705078125ms
+
+    console.time();
+    const x1 = wait(); // x1,y1,z1 同时异步执行， 包括setTimeout（10*1000）的执行时间
+    const y1 = wait(); // x1 与 setTimeout 属于同步执行
+    const z1 = wait();
+    await x1;
+    await y1;
+    await z1;
+    console.timeEnd(); // default: 10000.67822265625ms
+
+    console.time();
+    const x2 = wait(); // x2,y2,z2 同步执行，但是不包括setTimeout（10*1000）的执行时间
+    const y2 = wait(); // x2 与 setTimeout 属于异步执行
+    const z2 = wait();
+    x2, y2, z2;
+    console.timeEnd(); // default: 0.065185546875ms
+}
+main();
