86fe21c8c22502209002df53bff634eeecfe14c5
dajiaojiao <sir_weipengda@163.com>
2020-03-01 11:25:38 +0800

    文件调整

--- js/audio.js
+++
@@ -1,8 +0,0 @@
-var player;
-window.ispringPresentationConnector = {};
-window.ispringPresentationConnector.register = function (player) {
-    var presentation = player.presentation();
-    var uid = presentation.uniqueId();
-    console.log(player)
-};
-
