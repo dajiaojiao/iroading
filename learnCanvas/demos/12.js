ba183617528be2c59b16d4da6a7817b373f3a6ce
weipengda <weipengda@xueleyun.com>
2020-11-23 13:59:35 +0800

    橡皮擦clip实现

--- learnCanvas/demos/12.js
+++ learnCanvas/demos/12.js
@@ -1,5 +1,20 @@
+drawText();
+document.addEventListener("mousemove",function(event){
+    const {clientX,clientY} = event;
+    eraser(clientX,clientY);
+})
 function drawText(){
     context.save();
-    
+    context.strokeStyle = "#0ff";
+    context.font = "25px 宋体";
+    context.strokeText("人之所欲者无穷，而物之可以足吾欲者有尽",50,50);
+    context.restore();
+}
+function eraser(x,y){
+    context.save();
+    context.beginPath();
+    context.arc(x,y,10,0,Math.PI * 2,true);
+    context.clip();
+    context.clearRect(0,0,canvas.width,canvas.height);
     context.restore();
 }
